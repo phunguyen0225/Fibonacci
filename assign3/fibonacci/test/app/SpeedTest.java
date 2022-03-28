624ac895fe25962de9cfb291a6ba535686f607d1
Phu Nguyen <phunguyen0225@gmail.com>
2022-03-27 19:31:37 -0500

    SpeedTest and answer question in assign3.txt

--- assign3/fibonacci/test/app/SpeedTest.java
+++ assign3/fibonacci/test/app/SpeedTest.java
@@ -4,20 +4,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SpeedTest {
+  public long timeCalculation(Fibonacci fibonacci) {
+    long start = System.nanoTime();
+
+    fibonacci.fibonacciAtPosition(30);
+
+    long end = System.nanoTime();
+
+    return end - start;
+  }
+
   @Test
   public void compareSimpleAndMemoizedRecursion() {
-    long startTimeSimple = System.nanoTime();
-    new FibonacciSimpleRecursion();
-    long endTimeSimple = System.nanoTime();
-
-    long timeTakenSimple = endTimeSimple - startTimeSimple;
-
-    long startTimeMemoized = System.nanoTime();
-    new FibonacciMemoizedRecursion();
-    long endTimeMemoized = System.nanoTime();
-
-    long timeTakenMemoized = (endTimeMemoized - startTimeMemoized);
-
-    assertTrue(timeTakenSimple > timeTakenMemoized);
+    assertTrue(timeCalculation(new FibonacciSimpleRecursion()) > timeCalculation(new FibonacciMemoizedRecursion()) * 10);
   }
 }
